dbaee9e7fa621b7b43cc8a371a51976252c2ee0a
Tommo <tomcreer@gmail.com>
2020-12-05 18:34:44 +0000

    new files

--- streamlit_scanner.py
+++ streamlit_scanner.py
@@ -49,15 +49,17 @@ def load_data():
     
     #gdf_gullies.head()
     df = pd.read_parquet('scannerdata.parquet')
+    df_scrim = pd.read_parquet('scrim.parquet')
     #df = df.sort_values(['SECTIONLABEL','LABEL','STARTCH'])
-    return df
+    return [df, df_scrim]
 
 
-df = load_data()
+df, df_scrim = load_data()
 
 
 
 y = st.sidebar.selectbox("Road:", df['roadcode'].unique(), index=42)
+show_scrim = st.sidebar.checkbox('SCRIM results?')
 
 df2 = df[df['roadcode']==y]
 selected_chainage = st.slider('Chainage in m', int(df2['cumlength'].min()), int(df2['cumlength'].max()),  \
@@ -76,6 +78,9 @@ smoothing = st.sidebar.slider('Smoothing',0,20,(0))
 
 df3 = df2[(df2['SECTIONLABEL'] == 'CL1') & (df2['cumlength'] >= selected_chainage[0]) & (df2['cumlength'] <= selected_chainage[1])]
 df4 = df2[(df2['SECTIONLABEL'] == 'CR1') & (df2['cumlength'] >= selected_chainage[0]) & (df2['cumlength'] <= selected_chainage[1])]
+
+df3_scrim = df_scrim[(df_scrim['roadcode'] == y) & (df_scrim['XSP'] == 'CL1') & (df_scrim['cumlength'] >= selected_chainage[0]) & (df_scrim['cumlength'] <= selected_chainage[1])]
+df4_scrim = df_scrim[(df_scrim['roadcode'] == y) & (df_scrim['XSP'] == 'CR1') & (df_scrim['cumlength'] >= selected_chainage[0]) & (df_scrim['cumlength'] <= selected_chainage[1])]
 
 
 import folium
@@ -117,14 +122,6 @@ def plotDot(point):
                      , fill=True
                      ).add_to(feature_group2)
     
-feature_group3 = folium.FeatureGroup(name='Actual gullies', show=False)
-def plotGul(point):
-    folium.Circle( [point['X1'], point['Y1']], radius=2
-                     , color='darkgray'
-                     , fill_color='black'
-                     , fill=True
-                     ).add_to(feature_group3)
-
 feature_group5 = folium.FeatureGroup(name='Area of interest', show=True)
 def plotDot(point,color):
     folium.Circle( [point['X1'], point['Y1']], radius=2
@@ -151,8 +148,8 @@ def plotChain(point):
 
 df2.iloc[1::15].apply(lambda x: plotChain(x), axis = 1)
 
-df3.iloc[1::5].apply(lambda x: plotDot(x,'red'), axis = 1)
-df4.iloc[1::5].apply(lambda x: plotDot(x, 'green'), axis = 1)
+df3.iloc[1::5].apply(lambda x: plotDot(x,'blue'), axis = 1)
+df4.iloc[1::5].apply(lambda x: plotDot(x, 'red'), axis = 1)
 #if df3.shape[0] > df4.shape[0]:
 #    df3.iloc[1::10].apply(lambda x: plotChain(x), axis = 1)
 #else:
@@ -161,17 +158,16 @@ df4.iloc[1::5].apply(lambda x: plotDot(x, 'green'), axis = 1)
 #gdf_gullies.apply(lambda x: plotGul(x), axis = 1)
 
 #mapa.add_child(feature_group2)
-mapa.add_child(feature_group3)
 mapa.add_child(feature_group4)
 mapa.add_child(feature_group5)
 mapa.add_child(folium.map.LayerControl())
 folium_static(mapa)
 
 bands = {}
-bands[3] = {'LV3':[4,10], 'LV10':[21,56], 'LTRC':[0.15, 2.0], 'LLTX':[0.7, 0.4], 'LLRD':[10, 20], 'LRRD':[10, 20]}
-bands[4] = {'LV3':[5,13], 'LV10':[27,71], 'LTRC':[0.15, 2.0], 'LLTX':[0.6, 0.3], 'LLRD':[10, 20], 'LRRD':[10, 20]}
-bands[5] = {'LV3':[7,17], 'LV10':[35,93], 'LTRC':[0.15, 2.0], 'LLTX':[0.6, 0.3], 'LLRD':[10, 20], 'LRRD':[10, 20]}
-bands[6] = {'LV3':[8,20], 'LV10':[41,110], 'LTRC':[0.15, 2.0], 'LLTX':[0.6, 0.3], 'LLRD':[10, 20], 'LRRD':[10, 20]}
+bands[3] = {'LV3':[4,10], 'LV10':[21,56], 'LTRC':[0.15, 2.0], 'LLTX':[0.7, 0.4], 'LLRD':[10, 20], 'LRRD':[10, 20], 'RCI':[40, 100]}
+bands[4] = {'LV3':[5,13], 'LV10':[27,71], 'LTRC':[0.15, 2.0], 'LLTX':[0.6, 0.3], 'LLRD':[10, 20], 'LRRD':[10, 20], 'RCI':[40, 100]}
+bands[5] = {'LV3':[7,17], 'LV10':[35,93], 'LTRC':[0.15, 2.0], 'LLTX':[0.6, 0.3], 'LLRD':[10, 20], 'LRRD':[10, 20], 'RCI':[40, 100]}
+bands[6] = {'LV3':[8,20], 'LV10':[41,110], 'LTRC':[0.15, 2.0], 'LLTX':[0.6, 0.3], 'LLRD':[10, 20], 'LRRD':[10, 20], 'RCI':[40, 100]}
 
 import matplotlib.pyplot as plt
 def plotsir(add_text, description):
@@ -211,6 +207,80 @@ def plotsir(add_text, description):
   st.sidebar.pyplot(f)
 
 
+def plot_scrim_top():
+  f, ax = plt.subplots(1,1,figsize=(12,4))
+  #df3.plot(kind='line',x='cumlength',y='LV3',ax=ax)
+  if smoothing:
+   ax.plot(df3_scrim['cumlength'], df3_scrim['PARAMETER_'].rolling(smoothing).mean(), color='b', label='Left lane SCRIM coefficient')
+   ax.plot(df4_scrim['cumlength'], df4_scrim['PARAMETER_'].rolling(smoothing).mean(), color='r', label='Right lane SCRIM coefficient')
+   ax.plot(df3_scrim['cumlength'], df3_scrim['THRESHOLD_'], color='#abb0ff', label='Left lane threshold')
+   ax.plot(df4_scrim['cumlength'], df4_scrim['THRESHOLD_'], color='#ffabc1', label='Right lane threshold')
+  else:
+   ax.plot(df3_scrim['cumlength'], df3_scrim['PARAMETER_'], color='b', label='Left lane SCRIM coefficient')
+   ax.plot(df4_scrim['cumlength'], df4_scrim['PARAMETER_'], color='r', label='Right lane SCRIM coefficient') 
+   ax.plot(df3_scrim['cumlength'], df3_scrim['THRESHOLD_'], color='#abb0ff', label='Left lane threshold')
+   ax.plot(df4_scrim['cumlength'], df4_scrim['THRESHOLD_'], color='#ffabc1', label='Right lane threshold')      
+  #ax.plot(t, I, 'y', label='Right lane')
+  #ax.plot(t, R, 'g', alpha=0.7, linewidth=2, label='Radius')
+  
+  ax.set_yscale('linear')
+  ax.set_xlabel('Chainage  (m) - SCRIM coefficients')
+  #ax.set_ylabel('%')  # we already handled the x-label with ax1
+  #ax2 = ax.twinx()
+  color = 'tab:blue'
+  #ax2.set_ylabel('Radius (m)', color=color)  # we already handled the x-label with ax1
+  #ax2.set_yscale("log")
+  #ax2.plot(t, R, alpha=0.4, color=color,label='Radius')
+  #ax2.tick_params(axis='y', labelcolor=color)
+
+  #ax.yaxis.set_tick_params(length=0)
+  #ax.xaxis.set_tick_params(length=0)
+  #ax.grid(b=True, which='major', c='w', lw=2, ls='-')
+  legend = ax.legend()
+  legend.get_frame().set_alpha(0.5)
+  for spine in ('top', 'right', 'bottom', 'left'):
+      ax.spines[spine].set_visible(False)
+  st.sidebar.pyplot(f)
+
+def plot_scrim_bottom():
+  f, ax = plt.subplots(1,1,figsize=(12,4))
+  #df3.plot(kind='line',x='cumlength',y='LV3',ax=ax)
+  if smoothing:
+   ax.plot(df3_scrim['cumlength'], -df3_scrim['THRESHOLD1'].rolling(smoothing).mean(), color='b', label='Left lane SCRIM deficiency')
+   ax.plot(df4_scrim['cumlength'], -df4_scrim['THRESHOLD1'].rolling(smoothing).mean(), color='r', label='Right lane SCRIM deficiency')      
+  else:
+   ax.plot(df3_scrim['cumlength'], -df3_scrim['THRESHOLD1'], color='b', label='Left lane SCRIM deficiency')
+   ax.plot(df4_scrim['cumlength'], -df4_scrim['THRESHOLD1'], color='r', label='Right lane SCRIM deficiency')      
+
+
+  plt.axhline(y=0.0, color='#dbdbdb', linestyle='-')
+  plt.axhline(y=-0.1, color='#8a8a8a', linestyle='-')
+  plt.axhline(y=-0.2, color='#303030', linestyle='-')
+  
+  ax.set_yscale('linear')
+  ax.set_xlabel('Chainage  (m) - SCRIM deficiencies')
+  #ax.set_ylabel('%')  # we already handled the x-label with ax1
+  #ax2 = ax.twinx()
+  color = 'tab:blue'
+  #ax2.set_ylabel('Radius (m)', color=color)  # we already handled the x-label with ax1
+  #ax2.set_yscale("log")
+  #ax2.plot(t, R, alpha=0.4, color=color,label='Radius')
+  #ax2.tick_params(axis='y', labelcolor=color)
+
+  #ax.yaxis.set_tick_params(length=0)
+  #ax.xaxis.set_tick_params(length=0)
+  #ax.grid(b=True, which='major', c='w', lw=2, ls='-')
+  legend = ax.legend()
+  legend.get_frame().set_alpha(0.5)
+  for spine in ('top', 'right', 'bottom', 'left'):
+      ax.spines[spine].set_visible(False)
+  st.sidebar.pyplot(f)
+
+if show_scrim:
+  plot_scrim_top()
+  plot_scrim_bottom()
+
+
 for param in params_SELECTED:
     #st.write(param.split(' - ')[0])
     plotsir(param.split(' - ')[0], param.split(' - ')[1])
@@ -220,68 +290,3 @@ for param in params_SELECTED:
 #    df3 = df2[(df2['SECTIONLABEL'] == 'CL1') & (df2['X1'] >= bounds[0][0])  & (df2['X1'] <= bounds[1][0])  & (df2['Y1'] >= bounds[0][1])  & (df2['Y1'] <= bounds[1][1]) ]
 #    df4 = df2[(df2['SECTIONLABEL'] == 'CR1') & (df2['X1'] >= bounds[0][0])  & (df2['X1'] <= bounds[1][0])  & (df2['Y1'] >= bounds[0][1])  & (df2['Y1'] <= bounds[1][1]) ]
 #    folium_static(mapa)
-#    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-def RCI(rows, roadclass=None, urban=False):
-    #print(len(row.shape))
-    if len(rows.shape) > 1:
-        row = rows.iloc[0]
-    else:
-        row = rows
-    
-    if roadclass is None:
-        roadclass = row['LABEL'][0:1]
-        if roadclass not in 'ABCU':
-            roadclass = 'U'
-    
-    #Taken from UK Roads, SCANNER surveys for Local Roads, User Guide and Specification, Volume 3
-    llrt_or_lrrt_scoring = {'code':['LLRT','LRRT'],'A':[10,20], 'B':[10,20], 'C':[10,20],'U':[10,20],'max':100}
-    
-    lv3_scoring = {'code':'LV3', 'A':[4,10], 'B':[5,13], 'C':[7,17],'U':[8,20],'max':80}
-    
-    lv10_scoring = {'code':'LV10', 'A':[21,56], 'B':[27,71], 'C':[35,93],'U':[41,110],'max':60}   
-
-    ltrc_scoring = {'code':'LTRC', 'A':[0.15, 2.0], 'B':[0.15, 2.0], 'C':[0.15, 2.0],'U':[0.15, 2.0],'max':60}   
-
-    if urban:
-        if roadclass in ['A','B']:
-            lltx_scoring = {'code':'LLTX', 'A':[0.6, 0.3], 'B':[0.6, 0.3], 'C':[0.6,0.3],'U':[0.6,0.3],'max':50}   
-        else: #C,U
-            lltx_scoring = {'code':'LLTX', 'A':[0.6, 0.3], 'B':[0.6, 0.3], 'C':[0.6,0.3],'U':[0.6,0.3],'max':30}   
-    else:
-        if roadclass in ['A','B']:
-            lltx_scoring = {'code':'LLTX', 'A':[0.7, 0.4], 'B':[0.6, 0.3], 'C':[0.6,0.3],'U':[0.6,0.3],'max':75}   
-        else: #C,U
-            lltx_scoring = {'code':'LLTX', 'A':[0.7, 0.4], 'B':[0.6, 0.3], 'C':[0.6,0.3],'U':[0.6,0.3],'max':50}   
-        
-    
